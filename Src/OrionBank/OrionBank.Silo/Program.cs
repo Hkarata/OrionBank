3af9fc52f8edbfc314a93e56ec4066fb3f5d2dd9
hei karata <herishine00@gmail.com>
2024-08-04 11:11:36 +0300

    Add hosting and logging to OrionBank.Silo project
    Updated `OrionBank.Silo.csproj` to include `Microsoft.Extensions.Hosting` and `Microsoft.Extensions.Logging.Console` packages, both at version `8.0.0`. Modified `Program.cs` to set up a generic host using `Microsoft.Extensions.Hosting`, configure Orleans with localhost clustering, and run the application asynchronously.

--- Src/OrionBank/OrionBank.Silo/Program.cs
+++ Src/OrionBank/OrionBank.Silo/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.UseOrleans((context, siloBuilder) =>
+{
+    siloBuilder
+        .UseLocalhostClustering()
+        .ConfigureLogging(logging => logging.AddConsole());
+})
+    .UseConsoleLifetime();
+
+var app = builder.Build();
+
+await app.RunAsync();
